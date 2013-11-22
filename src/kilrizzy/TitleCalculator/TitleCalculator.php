73ad62c48c846836aacd2db80e322ca80d6723a4
Jeff Kilroy <jeffkilroy@gmail.com>
2013-11-22 11:36:24 -0500

    include settings

--- src/kilrizzy/TitleCalculator/TitleCalculator.php
+++ src/kilrizzy/TitleCalculator/TitleCalculator.php
@@ -2,10 +2,78 @@
 namespace kilrizzy\TitleCalculator;
 
 class TitleCalculator{
+
+	public $states;
+	public $values;
    
     public function __construct(){
-        //
+    	//Import settings
+    	include('settings.php');
+    	$this->states = $settings->states;
+    	//Setup default values
+        $this->values = new \StdClass();
+        $this->values->title_cost = 0;
     }
 
+    public function getData(){
+
+    }
 
 }
+//
+class PropertyState {
+    
+    public $name;
+    public $key;
+    public $types = array();
+
+    function __construct($key,$name) {
+    	$this->name = $name;
+    	$this->key = $key;
+    }
+
+} 
+//
+class PropertyType {
+    
+    public $name;
+    public $fees = array();
+    public $endorsements = array();
+
+    function __construct($name) {
+    	$this->name = $name;
+    }
+
+} 
+//
+class TypeFee {
+    
+    public $min;
+    public $max;
+    public $cost;
+    public $per;
+    
+    function __construct($min,$max,$cost,$per) {
+    	$this->min = $min;
+    	$this->max = $max;
+    	$this->cost = $cost;
+    	$this->per = $per;
+    }
+
+} 
+//
+class TypeEndorsement{
+    
+    public $name;
+    public $cost;
+    public $default;
+    public $editable;
+    
+    function __construct($name,$cost=0,$default=true,$editable=true) {
+    	$this->name = $name;
+    	$this->cost = $cost;
+    	$this->default = $default;
+    	$this->editable = $editable;
+    }
+
+} 
---
+++ src/kilrizzy/TitleCalculator/settings.php
@@ -0,0 +1,110 @@
+<?php
+use \kilrizzy\TitleCalculator\PropertyState as PropertyState;
+use \kilrizzy\TitleCalculator\PropertyType as PropertyType;
+use \kilrizzy\TitleCalculator\TypeFee as TypeFee;
+use \kilrizzy\TitleCalculator\TypeEndorsement as TypeEndorsement;
+$settings = new \StdClass();
+$settings->states = array();
+/*
+NJ
+*/
+$settings->states['NJ'] = new PropertyState("NJ","New Jersey");
+//PURCHASE
+//fees
+$settings->states['NJ']->types['purchase'] = new PropertyType("Purchase");
+$settings->states['NJ']->types['purchase']->fees[] = new TypeFee(0,100000,5.25,1000);
+$settings->states['NJ']->types['purchase']->fees[] = new TypeFee(100001,500000,4.25,1000);
+$settings->states['NJ']->types['purchase']->fees[] = new TypeFee(500001,2000000,2.75,1000);
+$settings->states['NJ']->types['purchase']->fees[] = new TypeFee(2000001,99000000,2,1000);
+//endorsements
+$settings->states['NJ']->types['purchase']->endorsements[] = new TypeEndorsement("Examination Fee",100);
+$settings->states['NJ']->types['purchase']->endorsements[] = new TypeEndorsement("Flood Search",10);
+$settings->states['NJ']->types['purchase']->endorsements[] = new TypeEndorsement("Upper Court Search",30);
+$settings->states['NJ']->types['purchase']->endorsements[] = new TypeEndorsement("Municipal Searches",48);
+$settings->states['NJ']->types['purchase']->endorsements[] = new TypeEndorsement("Alta 8.1 Endorsement",25);
+$settings->states['NJ']->types['purchase']->endorsements[] = new TypeEndorsement("Alta 9 Endorsement",25);
+$settings->states['NJ']->types['purchase']->endorsements[] = new TypeEndorsement("Survey",25);
+$settings->states['NJ']->types['purchase']->endorsements[] = new TypeEndorsement("Metes and Bounds",50);
+$settings->states['NJ']->types['purchase']->endorsements[] = new TypeEndorsement("Closing Services Letter",75);
+$settings->states['NJ']->types['purchase']->endorsements[] = new TypeEndorsement("Copies",15);
+$settings->states['NJ']->types['purchase']->endorsements[] = new TypeEndorsement("Tideland Search",42);
+$settings->states['NJ']->types['purchase']->endorsements[] = new TypeEndorsement("Overnight Charges",25);
+$settings->states['NJ']->types['purchase']->endorsements[] = new TypeEndorsement("Notice of Settlement",50);
+$settings->states['NJ']->types['purchase']->endorsements[] = new TypeEndorsement("Notary Fee",25);
+$settings->states['NJ']->types['purchase']->endorsements[] = new TypeEndorsement("E-Download Fee",50);
+$settings->states['NJ']->types['purchase']->endorsements[] = new TypeEndorsement("Simultanious Policy Fee",25);
+$settings->states['NJ']->types['purchase']->endorsements[] = new TypeEndorsement("Variable Rate Endorsement",25);
+$settings->states['NJ']->types['purchase']->endorsements[] = new TypeEndorsement("Condo Endorsement",25);
+$settings->states['NJ']->types['purchase']->endorsements[] = new TypeEndorsement("PUD Endorsement",25);
+$settings->states['NJ']->types['purchase']->endorsements[] = new TypeEndorsement("Secondary Rate Endorsement",25);
+$settings->states['NJ']->types['purchase']->endorsements[] = new TypeEndorsement("Settlement Fee",150);
+//REFINANCE
+//fees
+$settings->states['NJ']->types['refinance'] = new PropertyType("Refinance");
+$settings->states['NJ']->types['refinance']->fees[] = new TypeFee(0,100000,2.75,1000);
+$settings->states['NJ']->types['refinance']->fees[] = new TypeFee(100001,500000,2.5,1000);
+$settings->states['NJ']->types['refinance']->fees[] = new TypeFee(500001,2000000,2.25,1000);
+$settings->states['NJ']->types['refinance']->fees[] = new TypeFee(2000001,99000000,1.75,1000);
+//endorsements
+$settings->states['NJ']->types['refinance']->endorsements[] = new TypeEndorsement("Examination Fee",100);
+$settings->states['NJ']->types['refinance']->endorsements[] = new TypeEndorsement("Flood Search",10);
+$settings->states['NJ']->types['refinance']->endorsements[] = new TypeEndorsement("Upper Court Search",30);
+$settings->states['NJ']->types['refinance']->endorsements[] = new TypeEndorsement("Municipal Searches",48);
+$settings->states['NJ']->types['refinance']->endorsements[] = new TypeEndorsement("Alta 8.1 Endorsement",25);
+$settings->states['NJ']->types['refinance']->endorsements[] = new TypeEndorsement("Alta 9 Endorsement",25);
+$settings->states['NJ']->types['refinance']->endorsements[] = new TypeEndorsement("Survey",25);
+$settings->states['NJ']->types['refinance']->endorsements[] = new TypeEndorsement("Closing Services Letter",75);
+$settings->states['NJ']->types['refinance']->endorsements[] = new TypeEndorsement("Copies",15);
+$settings->states['NJ']->types['refinance']->endorsements[] = new TypeEndorsement("Overnight Charges",25);
+$settings->states['NJ']->types['refinance']->endorsements[] = new TypeEndorsement("Notice of Settlement",25);
+$settings->states['NJ']->types['refinance']->endorsements[] = new TypeEndorsement("Notary Fee",25);
+$settings->states['NJ']->types['refinance']->endorsements[] = new TypeEndorsement("E-Download Fee",50);
+$settings->states['NJ']->types['refinance']->endorsements[] = new TypeEndorsement("County Discharge Fee",75);
+$settings->states['NJ']->types['refinance']->endorsements[] = new TypeEndorsement("Payoff Overnight Fee",25);
+$settings->states['NJ']->types['refinance']->endorsements[] = new TypeEndorsement("Variable Rate Endorsement",25);
+$settings->states['NJ']->types['refinance']->endorsements[] = new TypeEndorsement("Condo Endorsement",25);
+$settings->states['NJ']->types['refinance']->endorsements[] = new TypeEndorsement("PUD Endorsement",25);
+$settings->states['NJ']->types['refinance']->endorsements[] = new TypeEndorsement("Secondary Rate Endorsement",25);
+$settings->states['NJ']->types['refinance']->endorsements[] = new TypeEndorsement("Settlement Fee",300);
+
+
+/*
+PA
+*/
+$settings->states['PA'] = new PropertyState("PA","Pennsylvania");
+//PURCHASE
+//fees
+$settings->states['PA']->types['purchase'] = new PropertyType("Purchase");
+$settings->states['PA']->types['purchase']->fees[] = new TypeFee(0,30000,500,0);
+$settings->states['PA']->types['purchase']->fees[] = new TypeFee(30001,45000,6.50,1000);
+$settings->states['PA']->types['purchase']->fees[] = new TypeFee(45001,100000,5.50,1000);
+$settings->states['PA']->types['purchase']->fees[] = new TypeFee(100001,500000,5.00,1000);
+$settings->states['PA']->types['purchase']->fees[] = new TypeFee(500001,1000000,4,1000);
+$settings->states['PA']->types['purchase']->fees[] = new TypeFee(1000001,2000000,3,1000);
+$settings->states['PA']->types['purchase']->fees[] = new TypeFee(2000001,7000000,2,1000);
+$settings->states['PA']->types['purchase']->fees[] = new TypeFee(7000001,30000000,1.5,1000);
+//endorsements
+$settings->states['PA']->types['purchase']->endorsements[] = new TypeEndorsement("Closing Services Letter",75);
+$settings->states['PA']->types['purchase']->endorsements[] = new TypeEndorsement("Notary Fee",20);
+$settings->states['PA']->types['purchase']->endorsements[] = new TypeEndorsement("Endorsements",125);
+$settings->states['PA']->types['purchase']->endorsements[] = new TypeEndorsement("Recording Fee",25);
+$settings->states['PA']->types['refinance']->endorsements[] = new TypeEndorsement("Settlement Fee",75);
+
+//REFINANCE
+//fees
+$settings->states['PA']->types['refinance'] = new PropertyType("Refinance");
+$settings->states['PA']->types['refinance']->fees[] = new TypeFee(0,30000,450,0);
+$settings->states['PA']->types['refinance']->fees[] = new TypeFee(30001,45000,5.25,1000);
+$settings->states['PA']->types['refinance']->fees[] = new TypeFee(45001,100000,4.75,1000);
+$settings->states['PA']->types['refinance']->fees[] = new TypeFee(100001,500000,4.25,1000);
+$settings->states['PA']->types['refinance']->fees[] = new TypeFee(500001,1000000,3.75,1000);
+$settings->states['PA']->types['refinance']->fees[] = new TypeFee(1000001,2000000,2.75,1000);
+$settings->states['PA']->types['refinance']->fees[] = new TypeFee(2000001,7000000,2,1000);
+$settings->states['PA']->types['refinance']->fees[] = new TypeFee(7000001,30000000,1.5,1000);
+//endorsements
+$settings->states['PA']->types['refinance']->endorsements[] = new TypeEndorsement("Closing Services Letter",75);
+$settings->states['PA']->types['refinance']->endorsements[] = new TypeEndorsement("Notary Fee",20);
+$settings->states['PA']->types['refinance']->endorsements[] = new TypeEndorsement("Endorsements",100);
+$settings->states['PA']->types['refinance']->endorsements[] = new TypeEndorsement("Tax and Utility ",80.50);
+$settings->states['PA']->types['refinance']->endorsements[] = new TypeEndorsement("Settlement Fee",75);
+?>
